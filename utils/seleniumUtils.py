330597f12eee017a3ac184c1cd67e95599d6d5ef
minwei35 <minwei1520@live.com>
2020-05-21 17:10:48 +0800

    新增系统统一日志工具类、selenium工具类

---
+++ utils/seleniumUtils.py
@@ -0,0 +1,61 @@
+#! /usr/local/env python
+# -*- encoding: utf-8 -*-
+
+"""
+@File  :  seleniumUtils.py
+@Time  :  2020-05-20 15:10
+@Author  :  jeremesang
+@Version :  V1.0
+@Desc    :  selenium工具类
+"""
+import logging
+from selenium import webdriver
+
+# 浏览器是否设置无头模式，仅测试时可以为False
+SetHeadless = True
+
+# 是否允许浏览器使用cookies
+EnableBrowserCookies = True
+
+
+def get_browser():
+    """
+    返回浏览器实例
+    """
+    # 设置selenium与urllib3的logger的日志等级为ERROR
+    # 如果不加这一步，运行爬虫过程中将会产生一大堆无用输出
+    logging.getLogger('selenium').setLevel('ERROR')
+    logging.getLogger('urllib3').setLevel('ERROR')
+
+    # selenium已经放弃了PhantomJS，开始支持firefox与chrome的无头模式
+    return use_chrome()
+
+
+def use_chrome():
+    """
+    使用selenium操作谷歌浏览器
+    """
+    options = webdriver.ChromeOptions()
+
+    # 下面一系列禁用操作是为了减少selenium的资源耗用，加速scrapy
+
+    profile = {
+        'profile.default_content_setting_values': {
+            # 禁用图片
+            'images': 2,
+            # 禁用浏览器弹窗
+            'notifications': 2
+        }
+    }
+    options.add_experimental_option('prefs', profile)
+
+    # 默认是无头模式，意思是浏览器将会在后台运行，也是为了加速scrapy
+    # 调试的时候可以把SetHeadless设为False，看一下跑着爬虫时候，浏览器在干什么
+    if SetHeadless:
+        # 无头模式，无UI
+        options.add_argument('-headless')
+
+    # 禁用gpu加速
+    options.add_argument('--disable-gpu')
+
+    return webdriver.Chrome(options=options)
---
+++ utils/stockLogger.py
@@ -0,0 +1,31 @@
+#! /usr/local/env python
+# -*- encoding: utf-8 -*-
+
+"""
+@File  :  stockLogger.py
+@Time  :  2020-05-21 14:21
+@Author  :  jeremesang
+@Version :  V1.0
+@Desc    :  日志输出模块
+"""
+
+import logging
+
+
+class StockLogger:
+    logger = None
+
+    def __init__(self, class_name):
+        self.logger = logging.getLogger(class_name)
+        self.logger.setLevel(logging.DEBUG)
+        # 建立一个streamHandler来把日志打在CMD窗口上
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        # 设置日志格式
+        formatter = logging.Formatter('[%(asctime)s] %(filename)s->%(funcName)s line:%(lineno)d [%(levelname)s]%(message)s')
+        ch.setFormatter(formatter)
+        # 将相应的handler添加在logger对象中
+        self.logger.addHandler(ch)
+
+    def get_logger(self):
+        return self.logger
