8821d1b3413e8f4f9954e0b7ce6e75e586cc44ee
minwei35 <minwei1520@live.com>
2020-05-22 17:36:39 +0800

    优化配置方式

---
+++ config/setting.py
@@ -0,0 +1,30 @@
+#! /usr/local/env python
+# -*- encoding: utf-8 -*-
+
+"""
+@File  :  setting.py
+@Time  :  2020-05-22 17:31
+@Author  :  jeremesang
+@Version :  V1.0
+@Desc    :  配置项
+"""
+
+""" 数据库配置 """
+DATABASES = {
+    "TYPE": "ORACLE",
+    "HOST": "192.168.10.80",
+    "PORT": "1521",
+    "SID": "orcl",
+    "USERNAME": "stock",
+    "PASSWORD": "stock"
+}
+
+""" 通用配置 """
+COMMON = {
+    # baostock的api在当前交易日17:30，完成日K线数据入库；本程序则是18点后进行日k数据入库
+    "DAY_K_DATA_TIME": "18",
+    # baostock的api在当前交易日20:30，完成分钟K线数据入库；本程序则是21点后进行分钟K数据入库
+    "MINUTE_K_DATA_TIME": "21",
+    # 多进程池的进程数
+    "POOL_COUNT": "8"
+}
