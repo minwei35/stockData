1d4da10f36d8222ba6ceaccd6e5066d4535acb1f
jeremysan <minwei1520@live.com>
2020-05-23 16:56:25 +0800

    修复在linux下sqlAlchemy编码出错的问题

--- database/sqlUtils.py
+++ database/sqlUtils.py
@@ -9,14 +9,14 @@ url = config.db_url
 
 
 def get_sqlalchemy_session():
-    engine = create_engine('oracle://{user}:{password}@{url}'.format(user=user, password=password, url=url), echo=False)
+    engine = create_engine('oracle://{user}:{password}@{url}?encoding=utf-8&nencoding=utf-8'.format(user=user, password=password, url=url), echo=False)
     session = sessionmaker(engine)
     my_session = session()
     return my_session
 
 
 def get_sqlalchemy_session_have_echo():
-    engine = create_engine('oracle://{user}:{password}@{url}'.format(user=user, password=password, url=url), echo=True)
+    engine = create_engine('oracle://{user}:{password}@{url}?encoding=utf-8&nencoding=utf-8'.format(user=user, password=password, url=url), echo=True)
     session = sessionmaker(engine)
     my_session = session()
     return my_session
